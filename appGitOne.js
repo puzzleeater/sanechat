f788c9482b6d270f3c23b1aa6f28128b1d375c8a
unknown <nonexistentunnamed@gmail.com>
2024-10-12 02:16:30 +0500

    new app.js with mock users hehe

--- appGitOne.js
+++ appGitOne.js
@@ -22,7 +22,7 @@ app.use(express.static("./public"));
 app.get("/", (req,res)=>{
 	const {user} = req.session;
 	if(!user) {
-		return res.status(301).setHeader("location", "/enter").send();
+		return res.setHeader("location", "/enter").status(301).send();
 	} else {
 		res.status(200).render("home", {user});
 	}
@@ -30,35 +30,54 @@ app.get("/", (req,res)=>{
 
 app.get("/enter", (req,res)=>{
 	const {user} = req.session;
-	if(user) return res.status(301).setHeader("location", "/").send();
+	if(user) return res.setHeader("location", "/").status(301).send();
 	res.status(200).render("enter", {data:null});
 });
 
+//temp users
+let tmpUsers = [
+	{id:1, username: "Jamal", email: "jamal@gmail.com", password:"1234"},
+	{id:2, username: "Lesa", email: "lesa@gmail.com", password:"4321"},
+	{id:3, username: "Raine", email: "raine@gmail.com", password:"010101"},
+	{id:4, username: "Nohn", email: "nohn@gmail.com", password:"990011aa11"}
+];
+//temp users
+
 app.post("/signup", (req,res)=>{
 	const {username, email, password} = req.body;
-	if(username.toLowerCase() == "system") return res.status(301).setHeader("location", "/enter").send();
+	if(username.toLowerCase() == "system") return res.setHeader("location", "/enter").status(301).send();
 	const user = {username,email,password};
 	createUser(user).then(result=>{
 		req.session.user = result;
 		req.session.save(err=>{
-			return res.status(301).setHeader("location", "/chat").send();
+			return res.setHeader("location", "/chat").status(301).send();
 		});
-	}).catch(err=>{return res.status(301).setHeader("location", "/").send();});
+	}).catch(err=>{return res.setHeader("location", "/").status(301).send();});
 });
 
 app.post("/signin", (req,res)=>{
 	const {email, password} = req.body;
 	const user = {email,password};
-	getUser(user).then(result=>{
+	let findTmp = tmpUsers.find((userx, index)=>userx.email==email&&userx.password==password);
+	console.log(findTmp, "zing");
+	if(findTmp) {
+		req.session.user = findTmp;
+		req.session.save(err=>{
+			res.setHeader("location", "/chat");
+			res.sendStatus(301);
+			return;
+		});
+	} else { return res.setHeader("location", "/").status(301).send(); }
+	/*getUser(user).then(result=>{
 		if(user) {
 			req.session.user = result;
 			req.session.save(err=>{
-				res.status(301).setHeader("location", "/chat").send();
+				res.setHeader("location", "/chat").status(301).send();
 			});
 		} else {
-			res.status(301).setHeader("location", "/").send();
+			res.setHeader("location", "/").status(301).send();
 		}
-	}).catch(err=>{ res.status(301).setHeader("location", "/").send(); });
+	}).catch(err=>{ res.setHeader("location", "/").status(301).send(); });*/
 });
 
 app.get("/clear", (req,res)=>{
@@ -70,10 +89,24 @@ app.get("/clear", (req,res)=>{
 app.get("/chat", (req,res)=>{
 	const {user} = req.session;
 	if(!user) {
-		res.status(301).setHeader("location", "/").send();
+		res.setHeader("location", "/").status(301).send();
 	} else {
 		res.status(200).render("chat", {user});
 	}
+});
+app.get("/test/:id", (req,res)=>{
+	new Promise((resolve,reject)=>{
+		let {id} = req.params;
+		if(id>0&&id<10) {
+			resolve(id);
+		} else {
+			reject("Failed");
+		}
+	}).then(result => {
+		res.status(200).send("<h1>Successful!</h1>");
+	}).catch(err => {
+		res.status(404).send("<h1>Failed!</h1>")
+	});
 });
 //app
 
@@ -91,21 +124,22 @@ io.on("connection", (socket)=>{
 	if(!user) {
 		socket.emit("message", "You are not logged in, Log in first!"); socket.disconnect();
 	} else {
-		getMessages().then(messages=>{
+		/*getMessages().then(messages=>{
 			for(let i = messages.length-1; i >= 0; i--) {
 				let message = messages[i];
 				socket.emit("message", message.message, message.username, message.msg_id, message.user_id);
 			}
-		}).catch(errx=>{console.log(errx)});
+		}).catch(errx=>{console.log(errx)});*/
 		
 		socket.on("disconnect", ()=>{
 			io.emit("message", `User ${user.username} disconnected!`, "SYSTEM");
 		});
 		socket.on("message", (message)=>{
-			createMessage(user, message).then(result=>{
+			/*createMessage(user, message).then(result=>{
 				//                 message, username, message id, user id
 				io.emit("message", message, user.username, result, user.id);
-			}).catch(err=>{console.log(err)});
+			}).catch(err=>{console.log(err)});*/
+			io.emit("message", message, user.username, 0/*result*/, user.id);
 		});
 		
 		socket.on("image", url=>{
